0f40ba8664653fe3efdac65ae96fb2d9a34f121d
mobinezzati662 <mobinezzati662@gmail.com>
2023-10-17 18:19:14 -0500

    Initial Commit

---
+++ LoginTest/LoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTestApp.swift
+//  LoginTest
+//
+//  Created by mobin on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
